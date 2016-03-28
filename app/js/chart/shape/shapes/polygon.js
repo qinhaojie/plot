aa19efcb968bfc7f89056753d6078835621b45b0
qinhaojie <qinhaojie1992@163.com>
2016-03-28 14:04:27 +0800

    多边形

---
+++ app/js/chart/shape/shapes/polygon.js
@@ -0,0 +1,22 @@
+import Line from './line.js';
+import util from '../../util.js';
+
+
+class Polygon extends Line{
+    constructor(chart,options){
+        options.color = '#ccc';
+        super(...arguments);
+    }
+    
+    getData() {
+        var p = this.getPoints();
+        var path = "M";
+        for (let [x, y] of p) {
+            path = path + x + ' ' + y + ' ';
+        }
+        path += 'z';
+        return path;
+    }
+} 
+
+export default Polygon;
